c2b719f9811d5d0a9ac4cae64596ee4a799b3183
Ashley Minshall <ashley.minshall@hashdata.co.za>
2020-08-04 17:37:47 +0200

    Fixed another typo on the stub and a bug with the scaffold command.

--- src/Commands/ScaffoldLivewireTableCommand.php
+++ src/Commands/ScaffoldLivewireTableCommand.php
@@ -15,8 +15,11 @@ class ScaffoldLivewireTableCommand extends FileManipulationCommand
 
     public function handle()
     {
-        $this->parser = new ComponentParser(config('livewire.class_namespace', 'App\\Http\\Livewire\\Tables'),
-            config('livewire.view_path', resource_path('views/livewire/tables')), $this->argument('name'));
+        $this->parser = new ComponentParser(
+            config('livewire.class_namespace', 'App\\Http\\Livewire\\Tables'),
+            config('livewire.view_path', resource_path('views/livewire/tables')),
+            $this->argument('name')
+        );
         $view = $this->createView();
         $view && $this->line('<options=bold,reverse;fg=green> Table Scaffolded </> ');
         $view && $this->line("<options=bold;fg=green>VIEW:</>  {$this->parser->relativeViewPath()}");
@@ -33,25 +36,37 @@ class ScaffoldLivewireTableCommand extends FileManipulationCommand
 
     protected function viewContents()
     {
-        $template = file_get_contents(__DIR__.DIRECTORY_SEPARATOR.'table.stub');
+        $template = file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . 'table.stub');
 
         // We need access to the table component class to retrieve and construct the fields and css
-        $tableComponent = new ReflectionClass($this->parser->baseClassNamespace.'\\'.$this->parser->className());
-        [$fields, $css] = $this->constructFieldsAndCss($tableComponent);
+        $className = $this->parser->baseClassNamespace . '\\' . $this->parser->className();
+
+        $tableComponent = new $className(1);
+        $css = $this->setCssArray($tableComponent->css);
+
+        $fields = $tableComponent->fields();
         [$patterns, $replacements] = $this->constructCssPatternsAndReplacements($css);
 
         // Replace the contents of the sub with header and data rows and css classes
-        return preg_replace('/\[header\]/', $this->headerRows($fields, $css),
-            preg_replace('/\[data\]/', $this->dataRows($fields, $css),
-                str_replace($patterns->toArray(), $replacements->toArray(), $template)));
+        return preg_replace(
+            '/\[header\]/',
+            $this->headerRows($fields, $css),
+            preg_replace(
+                '/\[data\]/',
+                $this->dataRows($fields, $css),
+                str_replace($patterns->toArray(), $replacements->toArray(), $template)
+            )
+        );
     }
 
     protected function headerRows(array $fields, array $css)
     {
         $cells = [];
         foreach ($fields as $key => $field) {
-            $cell = "\t\t\t<th".$this->getHeaderClass($field, $css).$this->getSortableAction($key,
-                    $field).'>'.$field['title'].'</th>';
+            $cell = "\t\t\t<th" . $this->getHeaderClass($field, $css) . $this->getSortableAction(
+                $key,
+                $field
+            ) . '>' . $field['title'] . '</th>';
             if (end($fields) !== $field) {
                 $cell .= PHP_EOL;
             }
@@ -65,8 +80,11 @@ class ScaffoldLivewireTableCommand extends FileManipulationCommand
     {
         $cells = [];
         foreach ($fields as $key => $field) {
-            $cell = "\t\t\t\t<td".$this->getDataClass($field, $css).'>{{ $row->'.str_replace('.', '->',
-                    $field['name']).' }}</td>';
+            $cell = "\t\t\t\t<td" . $this->getDataClass($field, $css) . '>{{ $row->' . str_replace(
+                '.',
+                '->',
+                $field['name']
+            ) . ' }}</td>';
             if (end($fields) !== $field) {
                 $cell .= PHP_EOL;
             }
@@ -80,11 +98,11 @@ class ScaffoldLivewireTableCommand extends FileManipulationCommand
     {
         if ((isset($css['td']) && $css['td'] && $css['td'] !== '') || (isset($field['cell_class']) && $field['cell_class'] && $field['cell_class'] !== '')) {
             if ((isset($css['td']) && $css['td'] && $css['td'] !== '') && (isset($field['cell_class']) && $field['cell_class'] && $field['cell_class'] !== '')) {
-                return ' class="'.$css['td'].' '.$field['cell_class'].'"';
+                return ' class="' . $css['td'] . ' ' . $field['cell_class'] . '"';
             } elseif (isset($css['td']) && $css['td'] && $css['td'] !== '') {
-                return ' class="'.$css['td'].'"';
+                return ' class="' . $css['td'] . '"';
             } else {
-                return ' class="'.$field['cell_class'].'"';
+                return ' class="' . $field['cell_class'] . '"';
             }
         }
     }
@@ -93,11 +111,11 @@ class ScaffoldLivewireTableCommand extends FileManipulationCommand
     {
         if ((isset($css['th']) && $css['th'] && $css['th'] !== '') || (isset($field['header_class']) && $field['header_class'] && $field['header_class'] !== '')) {
             if ((isset($css['th']) && $css['th'] && $css['th'] !== '') && (isset($field['header_class']) && $field['header_class'] && $field['header_class'] !== '')) {
-                return ' class="'.$css['th'].' '.$field['header_class'].'"';
+                return ' class="' . $css['th'] . ' ' . $field['header_class'] . '"';
             } elseif (isset($css['th']) && $css['th'] && $css['th'] !== '') {
-                return ' class="'.$css['th'].'"';
+                return ' class="' . $css['th'] . '"';
             } else {
-                return ' class="'.$field['header_class'].'"';
+                return ' class="' . $field['header_class'] . '"';
             }
         }
     }
@@ -105,7 +123,7 @@ class ScaffoldLivewireTableCommand extends FileManipulationCommand
     protected function getSortableAction($key, array $field)
     {
         if (isset($field['sortable']) && $field['sortable']) {
-            return ' wire:click="$emit(\'sortColumn\', '.$key.')"';
+            return ' wire:click="$emit(\'sortColumn\', ' . $key . ')"';
         }
     }
 
@@ -117,27 +135,14 @@ class ScaffoldLivewireTableCommand extends FileManipulationCommand
     {
         $patterns = collect(array_keys($css));
         $patterns->each(function ($pattern, $key) use ($patterns) {
-            $patterns[$key] = '['.$pattern.']';
+            $patterns[$key] = '[' . $pattern . ']';
         });
         $replacements = collect(array_values($css));
         $replacements->each(function ($replacement, $key) use ($replacements) {
-            ! is_null($replacement) ? $replacements[$key] = ' class="'.$replacement.'"' : $replacements[$key] = '';
+            !is_null($replacement) ? $replacements[$key] = ' class="' . $replacement . '"' : $replacements[$key] = '';
         });
 
         return [$patterns, $replacements];
-    }
-
-    /**
-     * @param  \ReflectionClass  $tableComponent
-     * @return array
-     */
-    protected function constructFieldsAndCss(ReflectionClass $tableComponent): array
-    {
-        $properties = $tableComponent->getDefaultProperties();
-        $fields = $properties['fields'];
-        $css = $this->setCssArray($properties['css']);
-
-        return [$fields, $css];
     }
 
     protected function setCssArray($css): array
